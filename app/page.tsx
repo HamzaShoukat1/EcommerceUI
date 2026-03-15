44a6e234590f09ee50ea57449f24cab7cbe0d139
hamza <=>
2026-03-16 04:18:52 +0500

    narbar footer products and mroe

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
-
-export default function page() {
+import Image from "next/image"
+import ProductList from "./src/components/shared/ProductList"
+export default function Homepage() {
   return (
-    <div>page</div>
+    <div>
+      <div className="relative aspect-3/1 mb-12">
+        <Image  src="/featured.png" alt="featured product" fill/>
+      </div>
+      <ProductList />
+
+    </div>
   )
 }
---
+++ app/src/components/shared/ProductCard.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import Link from "next/link";
+import { ProductType } from "../../types";
+import Image from "next/image";
+import { ShoppingCart } from "lucide-react";
+
+export default function ProductCard({ product }: { product: ProductType }) {
+  return (
+    <div className="shadow-lg rounded-lg overflow-hidden bg-white">
+      {/* Product Image */}
+      <Link href={`/products/${product.id}`}>
+        <div className="relative w-full aspect-2/3">
+          <Image
+            src={product.images[product.colors[0]]}
+            alt={product.name}
+            fill
+            className="object-cover hover:scale-105 transition-transform duration-300"
+          />
+        </div>
+      </Link>
+
+      {/* Product Details */}
+      <div className="flex flex-col gap-4 p-4">
+        <h1 className="font-medium truncate">{product.name}</h1>
+        <p className="text-sm text-gray-500">{product.shortDescription}</p>
+
+        {/* Size & Color Selection */}
+        <div className="flex items-center gap-4 text-xs">
+          {/* Size */}
+          <div className="flex flex-col gap-1 items-center">
+            <span className="text-gray-500">Size</span>
+            <select
+              name="size"
+              id="size"
+              className="cursor-pointer ring ring-gray-300 rounded-md px-2 py-1"
+            >
+              {product.sizes.map((size) => (
+                <option key={size} value={size} className="cursor-pointer">
+                  {size.toUpperCase()}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          {/* Colors */}
+          <div className="flex flex-col gap-1">
+            <span className="text-gray-500">Color</span>
+            <div className="flex items-center gap-2">
+              {product.colors.map((color) => (
+                <div key={color} className="">
+                  <div
+                    className="w-4 h-4 rounded-full border border-gray-300"
+                    style={{ backgroundColor: color }}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        {/* Price & Cart Button */}
+        <div className="flex items-center gap-2 justify-between mt-2">
+          <p className="font-medium">${product.price.toFixed(2)}</p>
+          <button className="flex items-center gap-2 bg-white text-black shadow-md text-xs rounded-md px-3 py-1  cursor-pointer hover:bg-black hover:text-white hover:ring-1 hover:ring-gray-400 transition-all duration-300">
+            <ShoppingCart className="w-4 h-4"/>
+            Add to Cart
+
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
